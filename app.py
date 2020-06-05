b56dc9e9a602c78182378068ab9119478f4e8a49
Henrique Abreu <abreu-semperfi@icloud.com>
2020-06-05 16:34:58 +0100

    new app functions + system tests

--- app.py
+++ app.py
@@ -1,14 +1,79 @@
-blogs_data = {}
-MENU_PROMPT = "Press 'c' for create, 'l' to list, 'r' to read, 'p' to post and 'q' to quit"
+import blog
+import post
+
+blogs_data = dict()
+MENU_PROMPT = "Press 'c' for create, 'l' to list, 'r' to read, 'p' to post and 'q' to quit: "
 
 
 def main_menu():
+    print_blogs()
     selection = input(MENU_PROMPT)
 
-    print_blogs()
+    while selection != "q":
+        if selection == "c":
+            create_blog()
+
+        elif selection == "p":
+            create_post()
+
+        elif selection == "r":
+            read_blog()
+
+        elif selection == "l":
+            print_blogs()
+
+        selection = input(MENU_PROMPT)
 
 
 def print_blogs():
     for key, value in blogs_data.items():
         print()
         print("- {}".format(value))
+
+
+def create_blog():
+    blog_title = input("Please insert your blog's title: ")
+    blog_author = input("Now please insert your pseudonym: ")
+
+    new_blog = blog.Blog(blog_title, blog_author)
+    blogs_data[blog_title] = new_blog
+
+
+def create_post():
+    post_title = input("Please insert your post's title: ")
+    post_content = input("Now write the post, press enter to submit: ")
+    blog_to_post = input("Where should we post this? ")
+
+    for key in blogs_data:
+        if blog_to_post == key:
+            blog_to_post = blogs_data[key]
+            blog_to_post.create_new_post(post_title, post_content)
+            break
+
+        raise (Exception, "blog not found")
+
+
+def read_blog():
+    blog_to_read = input("Select a blog to read: ")
+
+    for key in blogs_data:
+        if blog_to_read == key:
+            blog_to_read = blogs_data[key]
+            print_posts(blog_to_read)
+            break
+        raise (Exception, "blog not found")
+
+
+def print_posts(blog):
+    for item in blog.posts:
+        print_post(item)
+
+
+def print_post(post):
+    print('''
+    --------------------------
+    -{}
+    --------------------------
+    
+    {}
+    '''.format(post.title, post.content))
--- test_pack/system/app_test.py
+++ test_pack/system/app_test.py
@@ -1,15 +1,21 @@
 from unittest import TestCase
 from unittest.mock import patch
 import app
+import post
 from blog import Blog
 
 
 class AppTest(TestCase):
 
-    def test_print_blogs(self):
-
+    def create_test_blog(self):
         blog = Blog("My Test Blog", "Amazing Anonym Author")
         app.blogs_data = {"Test": blog}
+
+        return blog
+
+    def test_print_blogs(self):
+
+        blog = self.create_test_blog()
 
         with patch("builtins.print") as mocked_print:   ##patches print to see if blogs are printed
             app.print_blogs()
@@ -17,13 +23,53 @@ class AppTest(TestCase):
 
     def test_selection_promt(self):
 
-        with patch("builtins.input") as mocked_input:
+        with patch("builtins.input", return_value="q") as mocked_input:
             app.main_menu()
             mocked_input.assert_called_with(app.MENU_PROMPT)
 
     def test_menu_calls_print_blogs(self):
 
         with patch("app.print_blogs") as mocked_print:  ## patches input to check if func called
-            with patch("builtins.input"):               ## builtins.input, return_value= to return value
+            with patch("builtins.input", return_value="q"): ## builtins.input, return_value= to return value
                 app.main_menu()
                 mocked_print.assert_called()
+
+    def test_create_blog(self):
+
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = ("Test Blog", "Test Author")
+            app.create_blog()
+
+            self.assertIsNotNone(app.blogs_data.get("Test Blog"))
+
+    def test_read_blogs(self):
+        blog = self.create_test_blog()
+
+        with patch("builtins.input", return_value="Test"):
+            with patch("app.print_posts") as mocked_print_posts:
+                app.read_blog()
+
+                mocked_print_posts.assert_called_with(blog)
+
+    def test_print_posts(self):
+        blog = self.create_test_blog()
+        blog.create_new_post("Test Post", "Some Awesome Content")
+
+        with patch("app.print_post") as mocked_print_post:
+            app.print_posts(blog)
+            mocked_print_post.assert_called_with(blog.posts[0])
+
+    def test_print_post(self):
+        test_post = post.Post("Test Post Title", "Test Post Content")
+        expected_print = '''
+    --------------------------
+    -Test Post Title
+    --------------------------
+    
+    Test Post Content
+    '''
+
+        with patch("builtins.print") as mocked_print:
+            app.print_post(test_post)
+
+            mocked_print.assert_called_with(expected_print)
