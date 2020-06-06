c06a6399d178299fbafb696c72318a61450586cc
Henrique Abreu <abreu-semperfi@icloud.com>
2020-06-06 10:48:05 +0100

    new system tests for app.main_menu and app.create_post

--- test_pack/system/app_test.py
+++ test_pack/system/app_test.py
@@ -73,3 +73,59 @@ class AppTest(TestCase):
             app.print_post(test_post)
 
             mocked_print.assert_called_with(expected_print)
+
+    def test_create_post(self):
+        test = self.create_test_blog()
+
+        with patch("builtins.input") as mocked_input: ##Title, Content, blog in that order
+            mocked_input.side_effect = ("Test Post", "Test Content", "Test")
+            app.create_post()
+
+            self.assertEqual(app.blogs_data["Test"].posts[0].title, "Test Post")
+            self.assertEqual(app.blogs_data["Test"].posts[0].content, "Test Content")
+
+
+    ##main_menu test cases
+    def test_menu_calls_create_blog(self):
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = ("c", "new test blog", "new test author", "q")
+            with patch("app.create_blog") as mocked_create_blog:
+
+                app.main_menu()
+                mocked_create_blog.assert_called()
+
+    def test_menu_calls_create_post(self):
+        test = self.create_test_blog()
+
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = ("p", "new test post", "new test content", "Test", "q")
+            with patch("app.create_post") as mocked_create_post:
+
+                app.main_menu()
+                mocked_create_post.assert_called()
+
+    def test_menu_calls_read_blog(self):
+        test = self.create_test_blog()
+
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = "r", "Test", "q"
+            with patch("app.read_blog") as mocked_read_blog:
+
+                app.main_menu()
+                mocked_read_blog.assert_called()
+
+    def test_menu_calls_read_blogs(self):
+        test = self.create_test_blog()
+
+        with patch("builtins.input") as mocked_input:
+            mocked_input.side_effect = "l", "q"
+            with patch ("app.print_blogs") as mocked_print_blogs:
+
+                app.main_menu()
+                mocked_print_blogs.assert_called()
+
+
+
+
+
+
